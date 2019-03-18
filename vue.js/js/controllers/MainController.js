241a1c879da89b3e66b8aaaac75a11d4fa46c638
jinhyy <able0527@gmail.com>
2019-03-18 14:09:48 +0900

    2. 검색결과 구현

--- vue.js/js/controllers/MainController.js
+++ vue.js/js/controllers/MainController.js
@@ -36,6 +36,7 @@ export default {
 
     onResetForm(){
         console.log(tag,'onResetForm')
+        ResultView.hide()
     },
 
     onSearchResult(data){
--- vue.js/js/views/ResultView.js
+++ vue.js/js/views/ResultView.js
@@ -4,6 +4,9 @@ const tag ='[ResultView]'
 
 const ResultView=Object.create(View)
 
+ResultView.messages ={
+    NO_RESULT : '검색 결과가 없습니다.'
+}
 ResultView.setUp = function (el) {
     this.init(el)
 }
@@ -11,11 +14,23 @@ ResultView.setUp = function (el) {
 // 서버에서 받은 데이터를 받아 동적으로 dom을 그려주는 메소드
 ResultView.render = function (data= []) {
     console.log(tag, 'render()', data)
-    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : ' 검색 결과가 없습니다.'
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.messages.NO_RESULT
+    this.show()
 }
 
+// data가 컬렉션이므로 reduce함수로 html형식으로 표현
 ResultView.getSearchResultsHtml= function (data) {
-    debugger    // 데이터를 받고 디버거 호출
+    return data.reduce((html,item) => {
+        html += this.getSearchItemHtml(item)
+        return html
+    }, '<ul>') + '</ul>'
 }
 
+//  li 생성하는 함수, SearchModel에서 가져왔기 떄문에 이렇게 사용가능.
+ResultView.getSearchItemHtml = function (item) {
+    return `<li>
+    <img src="${item.image}">
+    <p>${item.name}</p>
+    </li>`
+}
 export default ResultView
