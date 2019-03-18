bdf57c9385f817cb72f2ba9b79b5ae3170084f42
jinhyy <able0527@gmail.com>
2019-03-18 17:17:53 +0900

    4. 추천 검색어 구현

--- vue.js/js/controllers/MainController.js
+++ vue.js/js/controllers/MainController.js
@@ -1,9 +1,10 @@
 import FormView from "../views/FormView.js";
 import ResultView from "../views/ResultView.js";
 import TabView from "../views/TabView.js";
+import KeywordView from "../views/KeywordView.js";
 
 import SearchModel from "../models/SearchModel.js";
-
+import KeywordModel from "../models/KeywordModel.js";
 
 const tag = '[MainController]' // 디버깅을 위하여 태그 설정,
  
@@ -23,24 +24,39 @@ export default {
         TabView.setUp(document.querySelector('#tabs'))
             .on('@change', e => this.onChangeTab(e.detail.tabName))
         this.selectedTab = '추천 검색어'
+        
+        KeywordView.setUp(document.querySelector('#search-keyword'))
+            .on('@click', e => this.onClickKeyword(e.detail.keyword))
         this.renderView()
     },
 
     renderView(){
       console.log(tag, 'renderView()') 
-      TabView.setActiveTab(this.selectedTab)
+      TabView.setActiveTab(this.selectedTab)    // default로 추천검색어 tab이 선택되게 한다.
+      if(this.selectedTab=== "추천 검색어"){
+          this.fetchSearchKeyword()
+      }
+      else{
+
+      }
       ResultView.hide()
+    },
+
+    fetchSearchKeyword(){
+        KeywordModel.list().then(data => {
+            KeywordView.render(data)
+        })
     },
 
     // 검색결과를 받아 해당하는 데이터를 onSearchResult 메소드에 넘겨한다.
     search(query){
         console.log(tag, 'onSearch()', query)
+        FormView.setValue(query)
         SearchModel.list(query).then(data =>
                 // list는 Promise 객체를 반환하기 때문에 then 메소드 사용가능
                // 넘어온 data를 onSearch로 넘겨준다.
-            this.onSearchResult(data)
-        )},
-
+            this.onSearchResult(data))
+        },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input)
@@ -49,14 +65,20 @@ export default {
 
     onResetForm(){
         console.log(tag,'onResetForm')
-        ResultView.hide()
+        this.renderView();
     },
 
     onSearchResult(data){
         ResultView.render(data)
+        KeywordView.hide()
+        TabView.hide()
     },
 
     onChangeTab(tabName){
         debugger
+    },
+
+    onClickKeyword(keyword){
+        this.search(keyword)
     }
 }
---
+++ vue.js/js/views/KeywordView.js
@@ -0,0 +1,42 @@
+import View from "./View.js";
+
+const tag ='[KeywordView]'
+
+const KeywordView=Object.create(View)
+
+KeywordView.messages = {
+    'NO_RESULT' : "추천 검색어가 없습니당."
+},
+
+KeywordView.setUp = function (el) {
+    this.init(el)
+    return this
+},
+
+KeywordView.render = function(data = []){
+    console.log(tag, 'render()', data)
+    this.el.innerHTML = data.length ? this.getKeywordsHtml(data) : this.messages.NO_RESULT
+    this.bindClick()
+    this.show()
+},
+
+KeywordView.bindClick = function(){
+    Array.from(this.el.querySelectorAll('li')).forEach(li=> {
+        li.addEventListener('click', e=> this.onClickKeyword(e))
+    })
+},
+
+//keyword 변수 객체에 전달받은 event.currentTarget.dataset을 받는다.
+KeywordView.onClickKeyword = function (e) {
+    const {keyword}= e.currentTarget.dataset
+    this.emit('@click',{keyword})
+},
+
+KeywordView.getKeywordsHtml = function (data) {
+    return data.reduce((html, item, index) => {
+      html += `<li data-keyword="${item.keyword}"><span class="number">${index + 1}</span>${item.keyword}</li>`
+      return html
+    }, '<ul class="list">') + "</ul>"
+  }
+
+export default KeywordView
