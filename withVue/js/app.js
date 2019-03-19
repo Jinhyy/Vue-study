7f307bd0a21f4af352534bcb49f0059b3cf9bcf2
jinhyy <able0527@gmail.com>
2019-03-19 13:05:54 +0900

    10. 추천, 최근검색어 구현

--- withVue/js/app.js
+++ withVue/js/app.js
@@ -1,5 +1,6 @@
 import SearchModel from "../models/SearchModel.js"
 import KeywordModel from "../models/KeywordModel.js"
+import HistoryModel from "../models/HistoryModel.js"
 
 new Vue ({
     el: '#app',// vue 인스턴스가 어느부분에 마운팅 될것인지 결정
@@ -8,17 +9,20 @@ new Vue ({
         searchResult: [],   // 검색결과 데이터를 저장
         tabs: ['추천 검색어','최근 검색어'],
         selectedTab: '',
+        keywords: [],
+        historys: [],
         submitted: false
     },
 
     created(){  // vue 인스턴스가 생성될 때 초기화해주는 예약 메소드.
         this.selectedTab=this.tabs[0]
+        this.fetchHistorys()
+        this.fetchKeywords()
     },
 
     methods: {
         onSubmit(e) {
-            this.search()
-            this.submitted=true
+            this.search(this.query)
         },
         onReset(){
             this.query=''
@@ -31,14 +35,41 @@ new Vue ({
         onKeyup(e) {
             if(this.query.length===0) this.onReset()
         },
-        search() {
+        search(query) {
             SearchModel.list().then(data => {
+                this.submitted=true
                 this.searchResult = data
+                HistoryModel.add(query)
+                this.fetchHistorys()
             })
         },
         onClickTab(tab) {
             this.selectedTab=tab
-        }
+        },
+        fetchKeywords(){
+            KeywordModel.list().then(data=> {
+                this.keywords = data
+            })
+        },
+        onClickKeyword(keyword){
+            this.query=keyword
+            this.search(this.query)
+        },
 
+        fetchHistorys(){
+            HistoryModel.list().then(data =>{
+                this.historys=data
+            })
+        },
+
+        onClickHistory(keyword){
+            this.query=keyword
+            this.search(this.query)
+        },
+
+        onClickRemoveHistory(keyword){
+            HistoryModel.remove(keyword)
+            this.fetchHistorys()
+        }
     }   
 })
