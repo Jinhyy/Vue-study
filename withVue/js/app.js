827969054a1fcceeceaa413877dfdfb88b6fc3ab
jinhyy <able0527@gmail.com>
2019-03-19 10:05:20 +0900

    7. vue 검색창 구현

--- withVue/js/app.js
+++ withVue/js/app.js
@@ -1,6 +1,19 @@
 new Vue ({
     el: '#app',// vue 인스턴스가 어느부분에 마운팅 될것인지 결정
     data: {
-        msg: 'hello world'
+        query: ''   // 입력 데이터를 받아 저장
+    },
+    methods: {
+        onSubmit(e) {
+            debugger
+        },
+        onReset(e) {
+            this.query=''
+            // todo 검색결과를 숨겨야함.
+            debugger
+        },
+        onKeyup(e) {
+            if(this.query.length===0) this.onReset()
+        }
     }
 })
