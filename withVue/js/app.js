b801fea72e5b579754d5a91fad94eb2cc8155306
jinhyy <able0527@gmail.com>
2019-03-19 11:08:27 +0900

    9. vue tab 구현

--- withVue/js/app.js
+++ withVue/js/app.js
@@ -1,12 +1,20 @@
 import SearchModel from "../models/SearchModel.js"
+import KeywordModel from "../models/KeywordModel.js"
 
 new Vue ({
     el: '#app',// vue 인스턴스가 어느부분에 마운팅 될것인지 결정
     data: {
         query: '',   // 입력 데이터를 받아 저장
-        searchResult: [],    // 검색결과 데이터를 저장
+        searchResult: [],   // 검색결과 데이터를 저장
+        tabs: ['추천 검색어','최근 검색어'],
+        selectedTab: '',
         submitted: false
     },
+
+    created(){  // vue 인스턴스가 생성될 때 초기화해주는 예약 메소드.
+        this.selectedTab=this.tabs[0]
+    },
+
     methods: {
         onSubmit(e) {
             this.search()
@@ -27,6 +35,10 @@ new Vue ({
             SearchModel.list().then(data => {
                 this.searchResult = data
             })
+        },
+        onClickTab(tab) {
+            this.selectedTab=tab
         }
+
     }   
 })
